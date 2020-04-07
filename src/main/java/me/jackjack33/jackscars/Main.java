b9df82e31f2c368a1c469bcd10cc5a72152fe542
SantioMC <SantioTheBest123@gmail.com>
2020-04-07 19:29:57 -0400

    Added Vault intergration

--- src/main/java/me/jackjack33/jackscars/Main.java
+++ src/main/java/me/jackjack33/jackscars/Main.java
@@ -5,19 +5,29 @@ import me.jackjack33.jackscars.Commands.JCHelp;
 
 import me.jackjack33.jackscars.Events.CarEvent;
 import me.jackjack33.jackscars.util.YAMLConfig;
+import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Bukkit;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.io.File;
 
 public class Main extends JavaPlugin {
 
     public YAMLConfig carConfig = new YAMLConfig(this.getDataFolder(), "data");
+    private static Economy economy = null;
 
     @Override
     public void onEnable() {
+
+        ConsoleCommandSender console = Bukkit.getConsoleSender();
+
+        // Vault Intergration
+        if (!setupEconomy()) {
+            String[] error = {"", "§4An error occurred whilst attempting to start up JacksCars", "§4Vault was not detected during startup! Please install the latest version @ https://www.spigotmc.org/resources/vault.34315/", ""};
+            console.sendMessage(error);
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
 
         carConfig.createConfig();
         this.saveDefaultConfig();
@@ -28,5 +38,23 @@ public class Main extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new CarEvent(this), this);
 
         Bukkit.getConsoleSender().sendMessage("JacksCars has started successfully.");
+    }
+
+    @Override
+    public void onDisable() {
+        carConfig.save();
+    }
+
+    // Vault-API: Economy
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        economy = rsp.getProvider();
+        return economy != null;
     }
 }
